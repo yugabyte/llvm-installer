59a09c1118a90ee2dbcbdd5bebc76e2767572f38
Mikhail Bautin <mbautin@users.noreply.github.com>
2023-04-04 17:44:10 -0700

    Update version to 1.2.6

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 
     setup(
         name='llvm-installer',
-        version='1.2.5',
+        version='1.2.6',
         url='https://github.com/yugabyte/llvm-installer',
         author='Mikhail Bautin',
         author_email='mbautin@users.noreply.github.com',
