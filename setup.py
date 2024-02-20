ada876fd9c24d8998560f17c581170098dc3f149
Mikhail Bautin <552936+mbautin@users.noreply.github.com>
2024-02-19 22:52:38 -0800

    Update version to 1.4.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 
     setup(
         name='llvm-installer',
-        version='1.4.0',
+        version='1.4.1',
         url='https://github.com/yugabyte/llvm-installer',
         author='Mikhail Bautin',
         author_email='mbautin@users.noreply.github.com',
